f3ad630d2ce7323ffaf288ed8ba14b10d20b01a7
Ramaa <ramaa196@gmail.com>
2018-11-10 18:01:01 -0300

    Creo la clase tablero como contenedora de una matriz

---
+++ src/fiuba/algo3/algoempires/Tablero.java
@@ -0,0 +1,30 @@
+package fiuba.algo3.algoempires;
+
+public class Tablero {
+	
+	private int [][] matriz =  new int[ANCHO][ALTO];;
+	static final int ANCHO = 20;
+	static final int ALTO = 20;
+	
+	public void inicializarTablero() {
+		for (int i=0; i<ANCHO; i++) {
+			for(int j=0; j<ALTO; j++) {
+				matriz[i][j] = 0;
+			}
+		}
+	}
+
+	public boolean estasVacio() {
+		for (int i=0; i<ANCHO; i++) {
+			for(int j=0; j<ALTO; j++) {
+				if(matriz[i][j] != 0) return false;
+			}
+		}
+		return true;
+	}
+
+	public void construir(Construible construible, int cordX, int cordY) {
+		construible.construiteEn(cordX,cordY);
+		
+	}
+}
