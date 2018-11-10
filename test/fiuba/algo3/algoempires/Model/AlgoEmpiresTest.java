43b6dc5eca927b9aa94f2e86f70ce0cffdac9032
Ramaa <ramaa196@gmail.com>
2018-11-10 18:01:45 -0300

    Agrego un par de test para empezar a trabajar

--- test/fiuba/algo3/algoempires/Model/AlgoEmpiresTest.java
+++ test/fiuba/algo3/algoempires/Model/AlgoEmpiresTest.java
@@ -8,8 +8,30 @@ public class AlgoEmpiresTest {
 	@Test
 	public void crearTableroLoInicializaVacio(){
 		AlgoEmpires algoEmpires = new AlgoEmpires();
-		algoEmpires.inicializarMapa();
+		algoEmpires.inicializarTablero();
         Assert.assertEquals(algoEmpires.elMapaEstaVacio(), true);
 	}
 	
+	@Test
+	public void crearTableroConUnEdificioNoEstaVacio() {
+		AlgoEmpires algoEmpires = new AlgoEmpires();
+		algoEmpires.inicializarTablero();
+		algoEmpires.agregarPlazaCentralInicial();
+		Assert.assertEquals(algoEmpires.elMapaEstaVacio(), false);
+	}
+	
+	@Test
+	public void siSeAgregaUnJugadorFaltaElOtro() {
+		AlgoEmpires algoEmpires = new AlgoEmpires();
+		algoEmpires.agregarJugadorConNombre("Pepito");
+		Assert.assertEquals(algoEmpires.totalDeJugadores(), 1);
+	}
+	
+	@Test
+	public void siSeAgreganDosJugadoresSePuedeEmpezarAJugar() {
+		AlgoEmpires algoEmpires = new AlgoEmpires();
+		algoEmpires.agregarJugadorConNombre("Pepito");
+		algoEmpires.agregarJugadorConNombre("Cosme");
+		Assert.assertEquals(algoEmpires.sePuedeEmpezarAJugar(), true);
+	}
 }
