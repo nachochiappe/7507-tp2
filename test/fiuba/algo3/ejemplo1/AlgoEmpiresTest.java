c0e461ba859c5108672451274e679538bfd83aaf
Ramaa <ramaa196@gmail.com>
2018-11-09 23:47:59 -0300

    Creo las clases

---
+++ test/fiuba/algo3/ejemplo1/AlgoEmpiresTest.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.ejemplo1;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class AlgoEmpiresTest {
+
+	@Test
+	public void crearTableroLoInicializaVacio(){
+
+	}
+	
+}
