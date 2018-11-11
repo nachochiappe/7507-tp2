aa691e71dbe62f42830c8163df520cc8508bf7c8
franco <franco.choquescipione@gmail.com>
2018-11-11 18:54:52 -0300

    intento arreglar build travis

--- test/fiuba/algo3/Entrega1/PruebasDeEdifcios.java
+++ test/fiuba/algo3/Entrega1/PruebasDeEdifcios.java
@@ -1,5 +1,11 @@
 package fiuba.algo3.Entrega1;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PruebasDeEdifcios {
+    @Test
+    public void prueba(){
+        boolean trueish = true;
+        Assert.assertEquals(trueish, true);
+    }
 }
