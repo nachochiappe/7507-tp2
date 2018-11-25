edf9941df5309912d11a50a34b940687065e031e
Ramaa <ramaa196@gmail.com>
2018-11-25 00:45:21 -0300

    Agrego vista aldeano con una imagen de un aldeano.

---
+++ src/fiuba/algo3/algoempires/Vista/VistaAldeano.java
@@ -0,0 +1,35 @@
+package fiuba.algo3.algoempires.Vista;
+
+import fiuba.algo3.algoempires.Model.EntidadesDelTablero.Unidades.Aldeano.Aldeano;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+
+public class VistaAldeano {
+
+	private Image vacio;
+    private Aldeano aldeano;
+    private Image graficoAldeano;
+    Canvas canvas;
+
+    public VistaAldeano(Aldeano aldeano, Canvas canvas) {
+        this.aldeano = aldeano;
+        this.canvas = canvas;
+        graficoAldeano = new Image("file:src/fiuba/algo3/algoempires/Vista/Imagenes/AldeanoConOro.png");
+        vacio = new Image("file:src/fiuba/algo3/algoempires/Vista/Imagenes/lugarVacio.png");
+    }
+
+    public void dibujar() {
+        this.clean();
+        canvas.getGraphicsContext2D().drawImage(graficoAldeano, aldeano.getPosicion().getPosicionX(), aldeano.getPosicion().getPosicionY());
+    }
+
+    public void clean() {
+        canvas.getGraphicsContext2D().drawImage(vacio, aldeano.getPosicion().getPosicionX(), aldeano.getPosicion().getPosicionY());
+    }
+
+    public void update() {
+        this.dibujar();
+    }
+}
