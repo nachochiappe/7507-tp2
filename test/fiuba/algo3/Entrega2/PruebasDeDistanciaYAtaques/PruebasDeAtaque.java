730d7b27539711996e71cbb347193d79e740adf0
Ramaa <ramaa196@gmail.com>
2018-12-20 22:50:43 -0300

    Pruebas de ataque pasando

--- test/fiuba/algo3/Entrega2/PruebasDeDistanciaYAtaques/PruebasDeAtaque.java
+++ test/fiuba/algo3/Entrega2/PruebasDeDistanciaYAtaques/PruebasDeAtaque.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.Entrega2.PruebasDeDistanciaYAtaques;
 
+import fiuba.algo3.algoempires.Model.Tablero;
 import fiuba.algo3.algoempires.Model.EntidadesDelTablero.Construibles.Edificios.Castillo;
 import fiuba.algo3.algoempires.Model.EntidadesDelTablero.Construibles.Edificios.Cuartel;
 import fiuba.algo3.algoempires.Model.EntidadesDelTablero.Construibles.Edificios.PlazaCentral;
@@ -24,15 +25,15 @@ public class PruebasDeAtaque {
 
 
 	@Test
-    public void testEspadachinAtacaPlazaCentralYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException{
-		Jugador jugador = Mockito.mock(Jugador.class);
+    public void testEspadachinAtacaPlazaCentralYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException, FueraDelMapaException, SoloSePermiteUnAldeanoException{
+		Tablero tablero = Tablero.getInstance();
+		tablero.inicializarTablero();
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion pos = new Posicion(3,3);
 		PlazaCentral plaza = new PlazaCentral();
-		try {
-			plaza.construiteEn(new Aldeano(jugador, pos), pos);
-		} catch (FueraDelMapaException | SoloSePermiteUnAldeanoException e) {
-			System.out.println("Error al construir la plaza de prueba");
-		}
+		plaza.establecerJugador(jugador);
+		plaza.construiteEn(new Aldeano(jugador, new Posicion(2,3)), pos);
+
 		Espadachin espadachin = new Espadachin(jugador, pos);
 		espadachin.atacar(plaza);
 		Assert.assertEquals(plaza.obtenerVida(), 35);
@@ -40,7 +41,7 @@ public class PruebasDeAtaque {
 	
 	@Test
     public void testEspadachinAtacaAOtroEspadachinYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException {
-		Jugador jugador = Mockito.mock(Jugador.class);
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion pos = new Posicion(4,4);
 		Espadachin espadachin = new Espadachin(jugador, pos);
 		Espadachin espadachinMalo = new Espadachin(jugador, pos);
@@ -60,35 +61,28 @@ public class PruebasDeAtaque {
 	}
 	
 	@Test(expected = ObjetivoFueraDeRangoException.class)
-    public void testEspadachinAtacaACastilloQueNoEstaCercaYLevantaExcepcion() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException {
+    public void testEspadachinAtacaACastilloQueNoEstaCercaYLevantaExcepcion() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException, FueraDelMapaException, SoloSePermiteUnAldeanoException {
+		Tablero tablero = Tablero.getInstance();
+		tablero.inicializarTablero();
 		Jugador player1 = new Jugador("Pepito");
 		Jugador player2 = new Jugador("Fulanito");
 		Posicion pos1 = new Posicion(0,0);
 		Posicion pos2 = new Posicion(0,3);
-		Espadachin espadachinBueno = new Espadachin (player1, pos1);
+		Espadachin espadachinBueno = new Espadachin (player2, pos1);
 		Castillo castilloMalo = new Castillo();
-		try {
-			castilloMalo.construiteEn(new Aldeano(player1, pos2), pos2);
-		} catch (FueraDelMapaException e) {
-			System.out.println("Error al construir el castillo de prueba");
-		} catch (SoloSePermiteUnAldeanoException e) {
-			e.printStackTrace();
-		}
+		castilloMalo.establecerJugador(player1);
+		castilloMalo.construiteEn(new Aldeano(player1, new Posicion(0,2)), pos2);
+
 		espadachinBueno.atacar(castilloMalo);
 	}
 	
 	@Test
-    public void testArqueroAtacaCuartelYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException{
-		Jugador jugador = Mockito.mock(Jugador.class);
+    public void testArqueroAtacaCuartelYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException, FueraDelMapaException, SoloSePermiteUnAldeanoException{
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion pos = new Posicion(4,4);
 		Cuartel cuartel = new Cuartel();
-		try {
-			cuartel.construiteEn(new Aldeano(jugador, pos), pos);
-		} catch (FueraDelMapaException e) {
-			System.out.println("Error al construir el cuartel de prueba");
-		} catch (SoloSePermiteUnAldeanoException e) {
-			e.printStackTrace();
-		}
+		cuartel.establecerJugador(jugador);
+		cuartel.construiteEn(new Aldeano(jugador, new Posicion(3,4)), pos);
 		Arquero arquero = new Arquero(jugador, pos);
 		arquero.atacar(cuartel);
 		Assert.assertEquals(cuartel.obtenerVida(), 40);
@@ -96,7 +90,7 @@ public class PruebasDeAtaque {
 	
 	@Test
     public void testArqueroAtacaAOtroArqueroYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException {
-		Jugador jugador = Mockito.mock(Jugador.class);
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion pos = new Posicion(4,4);
 		Arquero arquero = new Arquero(jugador, pos);
 		Arquero arqueroMalo = new Arquero(jugador, pos);
@@ -105,7 +99,7 @@ public class PruebasDeAtaque {
 	}
 	@Test
     public void testArqueroAtacaAUnEspadachinYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException {
-		Jugador jugador = Mockito.mock(Jugador.class);
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion pos = new Posicion(4,4);
 		Arquero arquero = new Arquero(jugador, pos);
 		Espadachin espadachinMalo = new Espadachin(jugador, pos);
@@ -115,7 +109,7 @@ public class PruebasDeAtaque {
 	
 	@Test
     public void testEspadachinAtacaAUnArqueroYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException {
-		Jugador jugador = Mockito.mock(Jugador.class);
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion pos = new Posicion(4,4);
 		Arquero arqueroMalo = new Arquero(jugador, pos);
 		Espadachin espadachin = new Espadachin(jugador, pos);
@@ -124,19 +118,16 @@ public class PruebasDeAtaque {
 	}
 	
 	@Test
-    public void testArmaDeAsedioMontadaAtacaCuartelYLeSacaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException{
-		Jugador jugador = Mockito.mock(Jugador.class);
+    public void testArmaDeAsedioMontadaAtacaCuartelYLeSacaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException, FueraDelMapaException, SoloSePermiteUnAldeanoException{
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion posicionArma = new Posicion(4,4);
 		Posicion posicionCastillo = new Posicion(9,4);
 		Castillo castilloMalo = new Castillo();
+		castilloMalo.establecerJugador(jugador);
 		ArmaDeAsedio armaDeAsedio = new ArmaDeAsedio(jugador, posicionArma);
-		try {
-			castilloMalo.construiteEn(new Aldeano(jugador, posicionArma), posicionCastillo);
-		} catch (FueraDelMapaException e) {
-			System.out.println("Error al construir el castillo de prueba");
-		} catch (SoloSePermiteUnAldeanoException e) {
-			e.printStackTrace();
-		}
+
+		castilloMalo.construiteEn(new Aldeano(jugador, posicionArma), posicionCastillo);
+
 		armaDeAsedio.toggleMontar();
 		armaDeAsedio.atacar(castilloMalo);
 		Assert.assertTrue(castilloMalo.obtenerVida()<0);
@@ -144,7 +135,7 @@ public class PruebasDeAtaque {
 
 	@Test (expected = ArmaDeAsedioNoAtacaUnidadesException.class)
     public void testArmaDeAsedioMontadaAtacaArqueroYLevantaExcepcion() throws ArmaDeAsedioNoAtacaUnidadesException, ObjetivoFueraDeRangoException, ArmaDeAsedioNoMontadaException {
-		Jugador jugador = Mockito.mock(Jugador.class);
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion posicionArma = new Posicion(4,4);
 		Posicion posicionArquero = new Posicion(9,4);
 		Arquero arqueroMalo = new Arquero(jugador, posicionArquero);
@@ -155,7 +146,7 @@ public class PruebasDeAtaque {
 	
 	@Test (expected = ArmaDeAsedioNoMontadaException.class)
     public void testArmaDeAsedioNoMontadaIntentaAtacarYLevantaExcepcion() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException {
-		Jugador jugador = Mockito.mock(Jugador.class);
+		Jugador jugador = new Jugador("JugadorTest");
 		Posicion posicionArma = new Posicion(4,4);
 		Posicion posicionArquero = new Posicion(9,4);
 		Arquero arqueroMalo = new Arquero(jugador, posicionArquero);
@@ -164,20 +155,18 @@ public class PruebasDeAtaque {
 	}
 	
 	@Test
-    public void testCastilloAtacaArqueroQueTieneEnRangoYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, OroInsuficienteException, PosicionOcupadaException {
+    public void testCastilloAtacaArqueroQueTieneEnRangoYLeBajaVida() throws ObjetivoFueraDeRangoException, ArmaDeAsedioNoAtacaUnidadesException, ArmaDeAsedioNoMontadaException, FueraDelMapaException, SoloSePermiteUnAldeanoException, OroInsuficienteException, PosicionOcupadaException {
+		Tablero tablero = Tablero.getInstance();
+		tablero.inicializarTablero();
 		Jugador player1 = new Jugador("Pepito");
 		Jugador player2 = new Jugador("Fulanito");
 		Posicion posEsp = new Posicion(0,0);
 		Posicion posCastillo = new Posicion(0,3);
 		Espadachin espadachinMalo = new Espadachin (player1, posEsp);
 		Castillo castillo = new Castillo();
-		try {
-			castillo.construiteEn(new Aldeano(player1, posCastillo), posCastillo);
-		} catch (FueraDelMapaException e) {
-			System.out.println("Error al construir el castillo de prueba");
-		} catch (SoloSePermiteUnAldeanoException e) {
-			e.printStackTrace();
-		}
+		castillo.establecerJugador(player1);
+		castillo.construiteEn(new Aldeano(player1, posCastillo), posCastillo);
+
 		castillo.atacar(espadachinMalo);
 		Assert.assertEquals(espadachinMalo.getHp(), 80);
 	}
